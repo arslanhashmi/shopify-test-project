ca4ca33f5338aafe2344e0101ac4a0ea76046b10
arslanhashmi <m.arslanijaz9@gmail.com>
2020-07-15 23:18:09 +0530

    Add api_call_with_retries

--- shopify_test_project/client/__init__.py
+++ shopify_test_project/client/__init__.py
@@ -1,3 +1,8 @@
 import logging
 
+import shopify
+
+from .config import SHOP_URL
+
 logging.basicConfig(level=logging.INFO)
+shopify.ShopifyResource.set_site(SHOP_URL)
--- shopify_test_project/client/wrappers.py
+++ shopify_test_project/client/wrappers.py
@@ -1,15 +1,26 @@
 import logging
+import time
 
 import shopify
+from pyactiveresource import connection
 
 from client.config import SHOP_URL
 
 logger = logging.getLogger(__name__)
 
-def api_call_with_retry():
-    shopify.ShopifyResource.set_site(SHOP_URL)
-    logger.info("Shop URL: [%s]", SHOP_URL)
+def api_call_with_retry(func, *args, **kwargs):
+    retries = kwargs.pop('retries', 3)
+    while retries >= 0:
+        try:
+            return func(*args, **kwargs)
+        except connection.ClientError as exc:
+            if exc.response.code == 429:
+                retry_after = float(exc.response.headers.get('Retry-After', 4))
+                logger.info(('Shopify API Rate Limit Exceeded.'
+                             'Retrying in %s seconds...'), retry_after)
+                time.sleep(retry_after)
+            elif exc.response.code >= 500:
+                retries -= 1
+            else:
+                raise exc
 
-    api_product = shopify.Product.find()[0]
-    api_product = shopify.Product.find()[1]
-    logger.info(api_product.title)
